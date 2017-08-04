5b9adaabd786ee26f18ab223dff46e3aacb87420
Myrle Krantz <myrle@apache.org>
2017-08-04 17:35:26 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Mifos I/O default setup
+
+[![Join the chat at https://gitter.im/mifos-initiative/mifos.io](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/mifos-initiative/mifos.io?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+This project provides resources which can be used to create a default setup for microservices in a Mifos I/O deployment. It is dependent on the apis and importers of those microservices.
+
+## Abstract
+Mifos I/O is an application framework for digital financial services, a system to support nationwide and cross-national financial transactions and help to level and speed the creation of an inclusive, interconnected digital economy for every nation in the world.
+
+## Steps needed to create a default setup for a new microservice 
+
+1.  Create a module with the same name as the microservice.  Add it to settings.gradle, and build.gradle of the composite build.
+
+2.  Place the csv files in subfolder of main/resources
+    
+3.  Change build.gradle of your module to reference the importer of your microservice in compileTest 
+
+4.  Write a test which checks the format of your csv files to prevent data corruption.  See accounting/ImportTest for an example of how to do this.
+
+## Versioning
+The version numbers follow the [Semantic Versioning](http://semver.org/) scheme.
+
+In addition to MAJOR.MINOR.PATCH the following postfixes are used to indicate the development state.
+
+* BUILD-SNAPSHOT - A release currently in development. 
+* M - A _milestone_ release include specific sets of functions and are released as soon as the functionality is complete.
+* RC - A _release candidate_ is a version with potential to be a final product, considered _code complete_.
+* RELEASE - _General availability_ indicates that this release is the best available version and is recommended for all usage.
+
+The versioning layout is {MAJOR}.{MINOR}.{PATCH}-{INDICATOR}[.{PATCH}]. Only milestones and release candidates can  have patch versions. Some examples:
+
+1.2.3-BUILD-SNAPSHOT  
+1.3.5-M.1  
+1.5.7-RC.2  
+2.0.0-RELEASE
+
+## License
+See [LICENSE](LICENSE) file.
---
+++ accounting/src/test/java/io/mifos/ImportTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2017 The Mifos Initiative.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.mifos;
+
+import io.mifos.accounting.api.v1.client.LedgerManager;
+import io.mifos.accounting.api.v1.domain.Account;
+import io.mifos.accounting.api.v1.domain.Ledger;
+import io.mifos.accounting.importer.AccountImporter;
+import io.mifos.accounting.importer.LedgerImporter;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.slf4j.Logger;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Myrle Krantz
+ */
+@RunWith(SpringRunner.class)
+public class ImportTest {
+  private Map<String, Ledger> createdLedgers = new HashMap<>();
+
+  @MockBean
+  LedgerManager ledgerManagerMock;
+
+  @MockBean
+  Logger loggerMock;
+
+  @Test
+  public void testStandardChartOfAccountsIsCorrectlyFormatted() throws IOException {
+    final LedgerImporter ledgerImporter = new LedgerImporter(ledgerManagerMock, loggerMock);
+    final URL ledgersUrl = ClassLoader.getSystemResource("standardChartOfAccounts/ledgers.csv");
+    Assert.assertNotNull(ledgersUrl);
+    ledgerImporter.importCSV(ledgersUrl);
+
+    final AccountImporter accountImporter = new AccountImporter(ledgerManagerMock, loggerMock);
+    final URL accountsUrl = ClassLoader.getSystemResource("standardChartOfAccounts/accounts.csv");
+    Assert.assertNotNull(accountsUrl);
+    accountImporter.importCSV(accountsUrl);
+  }
+
+  @Before
+  public void prepare() {
+    Mockito.doAnswer(new CollectCreatedLedgers(0)).when(ledgerManagerMock).createLedger(Matchers.any());
+    Mockito.doAnswer(new CollectCreatedLedgers(1)).when(ledgerManagerMock).addSubLedger(Matchers.any(), Matchers.any());
+    Mockito.doAnswer(new ReturnLedgers()).when(ledgerManagerMock).findLedger(Matchers.any());
+    Mockito.doAnswer(new ValidateArgument<>(0, Account.class)).when(ledgerManagerMock).createAccount(Matchers.any());
+  }
+
+  class CollectCreatedLedgers implements Answer {
+    private final int indexOfLedger;
+
+    CollectCreatedLedgers(int indexOfLedger) {
+      this.indexOfLedger = indexOfLedger;
+    }
+
+    @Override
+    public Void answer(InvocationOnMock invocation) throws Throwable {
+      final Ledger ledger = invocation.getArgumentAt(indexOfLedger, Ledger.class);
+
+      final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+      Validator validator = factory.getValidator();
+      final Set<ConstraintViolation<Ledger>> errors = validator.validate(ledger);
+      if (errors.size() > 0)
+        Assert.fail("Creation was requested for an invalid ledger.");
+
+      createdLedgers.put(ledger.getIdentifier(), ledger);
+      return null;
+    }
+  }
+
+  class ReturnLedgers implements Answer {
+    @Override
+    public Ledger answer(InvocationOnMock invocation) throws Throwable {
+      final String ledgerIdentifier = invocation.getArgumentAt(0, String.class);
+      return createdLedgers.get(ledgerIdentifier);
+    }
+  }
+
+  class ValidateArgument<T> implements Answer {
+    private final int indexOfArgument;
+    private final Class<T> thingyClass;
+
+    ValidateArgument(final int indexOfArgument, final Class<T> thingyClass) {
+      this.indexOfArgument = indexOfArgument;
+      this.thingyClass = thingyClass;
+    }
+
+    @Override
+    public Void answer(InvocationOnMock invocation) throws Throwable {
+      final T thingy = invocation.getArgumentAt(indexOfArgument, thingyClass);
+
+      final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+      Validator validator = factory.getValidator();
+      final Set<ConstraintViolation<T>> errors = validator.validate(thingy);
+      if (errors.size() > 0)
+        Assert.fail("Creation was requested for an invalid thingy.");
+
+      return null;
+    }
+  }
+}
